dcfb9e9664b9317306b5c4ddb1d302594f20d28f
Madhan-Sureshbabu <msureshbabu@wpi.edu>
2020-10-28 13:22:55 -0400

    added feature matching and compared scipy.least_squares with solvePnPRansac

--- HelperFunctions.py
+++ HelperFunctions.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python 
-
 import sys
 import cv2
 import numpy as np
--- odometry_functions_depthcompletion_dataset.py
+++ odometry_functions_depthcompletion_dataset.py
@@ -3,6 +3,7 @@
 import cv2
 import numpy as np
 from scipy.optimize import least_squares
+import matplotlib.pyplot as plt
 from math import *
 
 def genEulerZXZMatrix(psi, theta, sigma):
@@ -27,47 +28,27 @@ def genEulerZXZMatrix(psi, theta, sigma):
 
 	return mat
 
-def genEulerXYZMatrix(psi, theta, phi): # about x-psi, y-theta, z-phi
-	c1 = cos(psi)
-	s1 = sin(psi)
-	c2 = cos(theta)
-	s2 = sin(theta)
-	c3 = cos(phi)
-	s3 = sin(phi)
-
-	mat = np.zeros((3,3))
-
-	mat[0,0] = c3*c2
-	mat[0,1] = -s3*c1 + c3*s2*s1
-	mat[0,2] = s3*s1 + c3*s2*c1
-	mat[1,0] = s3*c2
-	mat[1,1] = c3*c1 + s3*s2*s1
-	mat[1,2] = -c3*s1 + s3*s2*c1
-	mat[2,0] = -s2
-	mat[2,1] = c2*s1
-	mat[2,2] = c2*c3
-
-	return mat
 
 def func(dof, pts_3d_1, pts_2d_2, P):
-	R = genEulerXYZMatrix(0, dof[0], 0)
-	t = np.array([[dof[1] * sin(dof[0]), 0, dof[1]*cos(dof[0])]]).reshape(3,1)
-	T = np.hstack((R,t))
-	T = np.vstack((T,[[0,0,0,1]]))
-	# print(T)
-	forward = np.matmul(P,np.linalg.inv(T))
-	pred_pts_2d_2 = []
-	residual = np.zeros((len(pts_3d_1),3))
-	# print(residual)
-	for i in range(len(pts_3d_1)):
-		pred_pts_2d_2.append(np.matmul(forward,pts_3d_1[i]))
-		pred_pts_2d_2[i] = pred_pts_2d_2[i]/pred_pts_2d_2[i][-1]
-		error = pts_2d_2[i] - pred_pts_2d_2[i]
-		residual[i,:] = error.reshape(1,3)[0]
-	sq_error = np.asarray([i*i for i in residual])
-	res = np.sum(sq_error,0)
-	res = np.asarray([res[0],res[1]])
-	return res
+    R = genEulerZXZMatrix(dof[0], dof[1], dof[2])
+    t = np.array([[dof[3], dof[4], dof[5]]]).reshape(3,1)
+    T = np.hstack((R,t))
+    T = np.vstack((T,[[0,0,0,1]]))
+    forward = np.matmul(P,np.linalg.inv(T))
+    residual = np.zeros((len(pts_3d_1),3))
+    pred_pts_2d_2 = []
+    for i in range(len(pts_3d_1)):
+        pred_pts_2d_2.append(np.matmul(forward,pts_3d_1[i]))
+        pred_pts_2d_2[i] = pred_pts_2d_2[i]/pred_pts_2d_2[i][-1]
+        # print(pred_pts_2d_2[i])
+        # print(pts_2d_2[i])
+        error = pts_2d_2[i] - pred_pts_2d_2[i]
+        residual[i,:] = error.reshape(1,3)[0]
+    # sq_error = np.asarray([i*i for i in residual])
+    # print(sq_error)
+    res = residual.flatten()
+    # print(res.shape)
+    return res
 
 
 # def find_SIFTKeypoints(image,lidar_image,maxNumFeatures):
@@ -79,7 +60,8 @@ def func(dof, pts_3d_1, pts_2d_2, P):
 
 def find_keypoints(image,lidar_image, feature_params):
 	gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-	mask_img = np.asarray(lidar_image,dtype=np.uint8)
+	mask_img = np.asarray(lidar_image,dtype=np.uint8) #cv2.inRange(lidar_image,lowerb=None,beta=0,aplha=np.amax(lidar_image),norm_type=cv2.NORM_MINMAX)
+	# plt.imshow(mask_img),plt.show()
 	features = cv2.goodFeaturesToTrack(gray_image, mask=mask_img, **feature_params)
 	return features
 
